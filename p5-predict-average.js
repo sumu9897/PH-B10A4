77bc7cdb96789f1ef7a27bad4ca6278a7861bb59
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-09-15 13:49:34 +0600

    Problem 5 Solve

---
+++ p5-predict-average.js
@@ -0,0 +1,19 @@
+function waitingTime(waitingTimes, serialNumber) {
+    if (!Array.isArray(waitingTimes) || typeof serialNumber !== "number") {
+        return "Invalid Input";
+    }
+
+    let totalTime = waitingTimes.reduce((acc, time) => acc + time, 0);
+    let avgTime = Math.round(totalTime / waitingTimes.length);
+
+    let remainingPeople = serialNumber - waitingTimes.length;
+    return remainingPeople > 0 ? remainingPeople * avgTime : 0;
+}
+
+console.log(waitingTime([3, 5, 7, 11, 6], 10));
+console.log(waitingTime([13, 2], 6));
+console.log(waitingTime([13, 2, 6, 7, 10], 6));
+console.log(waitingTime([6], 4));
+console.log(waitingTime(7, 10));
+console.log(waitingTime("[6,2]", 9));
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));
